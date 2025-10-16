d17c99c158610f0aa175b502902a468a62fd0bb8
sindhujapagadala <sindhujapagadala07@gmail.com>
2025-10-16 11:31:45 +0530

    updates the code

--- src/operation.py
+++
@@ -1,6 +0,0 @@
-def multiply(a,b):
-    return a+b
-def add(a,b):
-    return a+b
-def sub(a,b):
-    return a-b
--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,8 +1,10 @@
-from src.operation import sub,add
-def testadd():
+from src.math_operations import add,sub
+
+def test_add():
     assert add(2,3)==5
     assert add(-1,1)==0
-def testsub():
+    
+def test_sub():
     assert sub(5,3)==2
     assert sub(4,3)==1
     assert sub(3,3)==0
